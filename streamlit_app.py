065625bcf68f49425486483e094c4c395264863f
MrMikeDRogers <136319587+MrMikeDRogers@users.noreply.github.com>
2023-06-16 12:46:53 +0100

    vferg

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ def get_fruityvice_data(this_fruit_choice):
 
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
-        my_cur.execude("select * from fruit_load_list")
+        my_cur.execute("select * from fruit_load_list")
         return my_cur.fecthall()
 
 
