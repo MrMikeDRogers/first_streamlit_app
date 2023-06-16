b1ed103ea84405bf031f3a8ecd140b6a2e0a2dc4
MrMikeDRogers <136319587+MrMikeDRogers@users.noreply.github.com>
2023-06-16 12:32:34 +0100

    vrtb

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 import snowflake.connector
 from urllib.error import URLError
 
@@ -25,15 +26,19 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error('Please enter a fruit to get information')
+    else
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # flatten the json
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # write the flattened data out into a table
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# flatten the json
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write the flattened data out into a table
-streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
